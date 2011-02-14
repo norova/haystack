553cb41f3651ad71cd3406aed29d1eb37f91a1b5
norova <onezero.ss@gmail.com>
2011-02-14 01:54:44 -0600

    Initial commit.

---
+++ haystack/haystack.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace haystack
+{
+    partial class haystack
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(haystack));
+            this.timerSearch = new System.Windows.Forms.Timer(this.components);
+            this.buttonToggle = new System.Windows.Forms.Button();
+            this.timerSecond = new System.Windows.Forms.Timer(this.components);
+            this.labelRemain = new System.Windows.Forms.Label();
+            this.labelLastSearchEngine = new System.Windows.Forms.Label();
+            this.labelLastQuery = new System.Windows.Forms.Label();
+            this.textBoxLastQuery = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupSearchEngines = new System.Windows.Forms.GroupBox();
+            this.labelSelected = new System.Windows.Forms.Label();
+            this.labelAvailable = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.listSelected = new System.Windows.Forms.ListBox();
+            this.listAvailable = new System.Windows.Forms.ListBox();
+            this.groupSearchEngines.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timerSearch
+            // 
+            this.timerSearch.Tick += new System.EventHandler(this.timerSearch_Tick);
+            // 
+            // buttonToggle
+            // 
+            this.buttonToggle.Location = new System.Drawing.Point(12, 12);
+            this.buttonToggle.Name = "buttonToggle";
+            this.buttonToggle.Size = new System.Drawing.Size(199, 25);
+            this.buttonToggle.TabIndex = 0;
+            this.buttonToggle.Text = "Start Burying";
+            this.buttonToggle.UseVisualStyleBackColor = true;
+            this.buttonToggle.Click += new System.EventHandler(this.buttonToggle_Click);
+            // 
+            // timerSecond
+            // 
+            this.timerSecond.Interval = 1000;
+            this.timerSecond.Tick += new System.EventHandler(this.timerSecond_Tick);
+            // 
+            // labelRemain
+            // 
+            this.labelRemain.AutoSize = true;
+            this.labelRemain.Location = new System.Drawing.Point(12, 40);
+            this.labelRemain.Name = "labelRemain";
+            this.labelRemain.Size = new System.Drawing.Size(98, 13);
+            this.labelRemain.TabIndex = 5;
+            this.labelRemain.Text = "Next search in: Idle";
+            // 
+            // labelLastSearchEngine
+            // 
+            this.labelLastSearchEngine.AutoSize = true;
+            this.labelLastSearchEngine.Location = new System.Drawing.Point(12, 53);
+            this.labelLastSearchEngine.Name = "labelLastSearchEngine";
+            this.labelLastSearchEngine.Size = new System.Drawing.Size(132, 13);
+            this.labelLastSearchEngine.TabIndex = 6;
+            this.labelLastSearchEngine.Text = "Last Search Engine: None";
+            // 
+            // labelLastQuery
+            // 
+            this.labelLastQuery.AutoSize = true;
+            this.labelLastQuery.Location = new System.Drawing.Point(12, 72);
+            this.labelLastQuery.Name = "labelLastQuery";
+            this.labelLastQuery.Size = new System.Drawing.Size(61, 13);
+            this.labelLastQuery.TabIndex = 7;
+            this.labelLastQuery.Text = "Last Query:";
+            // 
+            // textBoxLastQuery
+            // 
+            this.textBoxLastQuery.Location = new System.Drawing.Point(12, 88);
+            this.textBoxLastQuery.Multiline = true;
+            this.textBoxLastQuery.Name = "textBoxLastQuery";
+            this.textBoxLastQuery.Size = new System.Drawing.Size(199, 111);
+            this.textBoxLastQuery.TabIndex = 8;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "haystack : left = restore / right = exit";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // groupSearchEngines
+            // 
+            this.groupSearchEngines.Controls.Add(this.labelSelected);
+            this.groupSearchEngines.Controls.Add(this.labelAvailable);
+            this.groupSearchEngines.Controls.Add(this.buttonAdd);
+            this.groupSearchEngines.Controls.Add(this.buttonRemove);
+            this.groupSearchEngines.Controls.Add(this.listSelected);
+            this.groupSearchEngines.Controls.Add(this.listAvailable);
+            this.groupSearchEngines.Location = new System.Drawing.Point(217, 12);
+            this.groupSearchEngines.Name = "groupSearchEngines";
+            this.groupSearchEngines.Size = new System.Drawing.Size(264, 187);
+            this.groupSearchEngines.TabIndex = 13;
+            this.groupSearchEngines.TabStop = false;
+            this.groupSearchEngines.Text = "Search Engines To Use";
+            // 
+            // labelSelected
+            // 
+            this.labelSelected.AutoSize = true;
+            this.labelSelected.Location = new System.Drawing.Point(157, 16);
+            this.labelSelected.Name = "labelSelected";
+            this.labelSelected.Size = new System.Drawing.Size(49, 13);
+            this.labelSelected.TabIndex = 18;
+            this.labelSelected.Text = "Selected";
+            // 
+            // labelAvailable
+            // 
+            this.labelAvailable.AutoSize = true;
+            this.labelAvailable.Location = new System.Drawing.Point(3, 16);
+            this.labelAvailable.Name = "labelAvailable";
+            this.labelAvailable.Size = new System.Drawing.Size(50, 13);
+            this.labelAvailable.TabIndex = 17;
+            this.labelAvailable.Text = "Available";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(110, 50);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(44, 20);
+            this.buttonAdd.TabIndex = 15;
+            this.buttonAdd.Text = "-->";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(110, 76);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(44, 20);
+            this.buttonRemove.TabIndex = 16;
+            this.buttonRemove.Text = "<--";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // listSelected
+            // 
+            this.listSelected.FormattingEnabled = true;
+            this.listSelected.Location = new System.Drawing.Point(160, 32);
+            this.listSelected.Name = "listSelected";
+            this.listSelected.Size = new System.Drawing.Size(98, 147);
+            this.listSelected.Sorted = true;
+            this.listSelected.TabIndex = 14;
+            this.listSelected.DoubleClick += new System.EventHandler(this.listSelected_DoubleClick);
+            // 
+            // listAvailable
+            // 
+            this.listAvailable.FormattingEnabled = true;
+            this.listAvailable.Location = new System.Drawing.Point(6, 32);
+            this.listAvailable.Name = "listAvailable";
+            this.listAvailable.Size = new System.Drawing.Size(98, 147);
+            this.listAvailable.Sorted = true;
+            this.listAvailable.TabIndex = 13;
+            this.listAvailable.DoubleClick += new System.EventHandler(this.listAvailable_DoubleClick);
+            // 
+            // haystack
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 208);
+            this.Controls.Add(this.groupSearchEngines);
+            this.Controls.Add(this.textBoxLastQuery);
+            this.Controls.Add(this.labelLastQuery);
+            this.Controls.Add(this.labelLastSearchEngine);
+            this.Controls.Add(this.labelRemain);
+            this.Controls.Add(this.buttonToggle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "haystack";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "haystack";
+            this.Load += new System.EventHandler(this.haystack_Load);
+            this.Resize += new System.EventHandler(this.haystack_Resize);
+            this.groupSearchEngines.ResumeLayout(false);
+            this.groupSearchEngines.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerSearch;
+        private System.Windows.Forms.Button buttonToggle;
+        private System.Windows.Forms.Timer timerSecond;
+        private System.Windows.Forms.Label labelRemain;
+        private System.Windows.Forms.Label labelLastSearchEngine;
+        private System.Windows.Forms.Label labelLastQuery;
+        private System.Windows.Forms.TextBox textBoxLastQuery;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.GroupBox groupSearchEngines;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.ListBox listSelected;
+        private System.Windows.Forms.ListBox listAvailable;
+        private System.Windows.Forms.Label labelSelected;
+        private System.Windows.Forms.Label labelAvailable;
+    }
+}
+
---
+++ haystack/haystack.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace haystack
+{
+    public partial class haystack : Form
+    {
+        public bool DEBUG = false;
+
+        public Random random = new Random();
+        
+        public int remain;
+        public string lastSearchEngine;
+        public string lastQuery;
+
+        public const int TIME_MIN = 15;
+        public const int TIME_MAX = 900;
+
+        public haystack()
+        {
+            InitializeComponent();
+            timerSearch.Stop();
+            timerSecond.Stop();
+        }
+
+        public string BuildQuery(string WordListFile, int MaxWords)
+        {
+            string Query = null;
+
+            FileStream FS = new FileStream(WordListFile, FileMode.Open, FileAccess.Read, FileShare.Read);
+            StreamReader SR = new StreamReader(FS);
+
+            string text = SR.ReadToEnd();
+
+            Regex RE = new Regex("\n", RegexOptions.Multiline);
+            MatchCollection matches = RE.Matches(text);
+
+            int numSearchTerms = random.Next(1, MaxWords);
+
+            for (int i = 0; i < numSearchTerms; i++)
+            {
+                string temp = null;
+                SR.DiscardBufferedData();
+                SR.BaseStream.Position = random.Next(0, (int)SR.BaseStream.Length);
+                temp = SR.ReadLine();
+                temp = SR.ReadLine();
+
+                Query += temp + " ";
+            }
+
+            SR.Close();
+            FS.Close();
+
+            return Query.Trim();
+        }
+
+        public void SendSearchQuery(string SearchEngine, string Query)
+        {
+            string Parameters = null;
+            string url = null;
+
+            Query = Query.Trim();
+
+            switch (SearchEngine)
+            {
+                case "Google":
+                    Parameters = "hl=en";
+                    Parameters += "&q=" + Query;
+                    Parameters += "&btnG=Google+Search";
+
+                    url = "http://www.google.com/search?" + Parameters;
+                    break;
+
+                case "Ask":
+                    Parameters = "q=" + Query;
+                    Parameters += "&search=search";
+                    Parameters += "&qsrc=0";
+                    Parameters += "&o=0";
+                    Parameters += "&1=dir";
+
+                    url = "http://www.ask.com/web?" + Parameters;
+                    break;
+
+                case "Yahoo":
+                    Parameters = "p=" + Query;
+                    Parameters += "&fr=yfp-t-501";
+                    Parameters += "&toggle=1";
+                    Parameters += "&cop=mss";
+                    Parameters += "&ei=UTF-8";
+
+                    url = "http://search.yahoo.com/search?" + Parameters;
+                    break;
+
+                case "AltaVista":
+                    Parameters = "itag=ody";
+                    Parameters += "&q=" + Query;
+                    Parameters += "&kgs=1";
+                    Parameters += "&kls=0";
+
+                    url = "http://www.altavista.com/web/results?" + Parameters;
+                    break;
+
+                case "Live Search":
+                    Parameters = "q=" + Query;
+                    Parameters += "&go=";
+                    Parameters += "&form=QBHP";
+
+                    url = "http://search.live.com/results.aspx?" + Parameters;
+                    break;
+
+                case "metacrawler":
+                    url = "http://www.metacrawler.com/info.metac/search/web/" + Query;
+                    break;
+
+                case "Excite":
+                    url = "http://msxml.excite.com/info.xcite/search/web/" + Query.Replace(" ", "%2B");
+                    break;
+
+                case "Answers.com":
+                    url = "http://www.answers.com/" + Query;
+                    break;
+
+                case "WebMD":
+                    Parameters = "query=" + Query;
+                    Parameters += "&sourceType=undefined";
+
+                    url = "http://www.webmd.com/search/search_results/default.aspx?" + Parameters;
+                    break;
+            }
+
+            WebResponse response = GetResponse(url);
+            if (response == null)
+                return;
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string content = reader.ReadToEnd();
+
+            lastSearchEngine = SearchEngine;
+            lastQuery = Query;
+
+            if (DEBUG)
+            {
+                StreamWriter os = new StreamWriter(Environment.GetEnvironmentVariable("TEMP") + @"\haystack_out.html");
+                os.Write(content);
+                os.Close();
+                System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Mozilla Firefox\firefox.exe", "file:///" + Environment.GetEnvironmentVariable("TEMP").Replace("\\", "/") + "/haystack_out.html");
+            }
+        }
+
+        private WebResponse GetResponse(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.AllowAutoRedirect = false;
+                request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+                return request.GetResponse();
+            }
+            // Catch exception if trying to set a restricted header.
+            catch (ArgumentException e)
+            {
+                MessageBox.Show("ArgumentException is thrown. Message is: " + e.Message);
+                return null;
+            }
+        }
+
+        private void buttonToggle_Click(object sender, EventArgs e)
+        {
+            if (!timerSearch.Enabled)
+            {
+                if (listSelected.Items.Count <= 0)
+                {
+                    MessageBox.Show("You must select at least one search engine to use.");
+                    return;
+                }
+
+                buttonToggle.Text = "Stop Burying";
+                timerSearch.Interval = random.Next(TIME_MIN, TIME_MAX) * 1000;
+                remain = timerSearch.Interval;
+                timerSearch.Start();
+                timerSecond.Start();
+            }
+            else
+            {
+                buttonToggle.Text = "Start Burying";
+                labelRemain.Text = "Next search in: Idle";
+                timerSearch.Stop();
+                timerSecond.Stop();
+            }
+        }
+
+        private void timerSearch_Tick(object sender, EventArgs e)
+        {
+            timerSearch.Interval = random.Next(TIME_MIN, TIME_MAX) * 1000;
+            remain = timerSearch.Interval;
+
+            int engine = random.Next(0, listSelected.Items.Count);
+            SendSearchQuery(listSelected.Items[engine].ToString(), BuildQuery("wordlist", 15));
+
+            labelLastSearchEngine.Text = "Last Search Engine: " + lastSearchEngine;
+            textBoxLastQuery.Text = lastQuery;
+        }
+
+        private void timerSecond_Tick(object sender, EventArgs e)
+        {
+            remain -= 1000;
+            labelRemain.Text = "Next search in: " + (remain / 1000) / 60 + "m" + (remain / 1000) % 60 + "s";
+            labelRemain.Refresh();
+
+            if (this.WindowState == FormWindowState.Minimized)
+                notifyIcon.Text = "haystack (next: " + (remain / 1000) / 60 + "m" + (remain / 1000) % 60 + "s) : left = restore / right = exit";
+        }
+
+        private void haystack_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                this.Visible = true;
+                this.WindowState = FormWindowState.Normal;
+                notifyIcon.Visible = false;
+            }
+        }
+
+        private void haystack_Load(object sender, EventArgs e)
+        {
+            listAvailable.Items.Add("Google");
+            listAvailable.Items.Add("Ask");
+            listAvailable.Items.Add("Yahoo");
+            listAvailable.Items.Add("AltaVista");
+            listAvailable.Items.Add("Live Search");
+            listAvailable.Items.Add("metacrawler");
+            listAvailable.Items.Add("Excite");
+            listAvailable.Items.Add("Answers.com");
+            listAvailable.Items.Add("WebMD");
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (listAvailable.SelectedIndex >= 0)
+            {
+                listSelected.Items.Add(listAvailable.SelectedItem.ToString());
+                listAvailable.Items.RemoveAt(listAvailable.SelectedIndex);
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (listSelected.SelectedIndex >= 0)
+            {
+                listAvailable.Items.Add(listSelected.SelectedItem.ToString());
+                listSelected.Items.RemoveAt(listSelected.SelectedIndex);
+            }
+        }
+
+        private void listAvailable_DoubleClick(object sender, EventArgs e)
+        {
+            if (listAvailable.Items.Count > 0)
+            {
+                listSelected.Items.Add(listAvailable.SelectedItem.ToString());
+                listAvailable.Items.RemoveAt(listAvailable.SelectedIndex);
+            }
+        }
+
+        private void listSelected_DoubleClick(object sender, EventArgs e)
+        {
+            if (listSelected.Items.Count > 0)
+            {
+                listAvailable.Items.Add(listSelected.SelectedItem.ToString());
+                listSelected.Items.RemoveAt(listSelected.SelectedIndex);
+            }
+        }
+    }
+}
